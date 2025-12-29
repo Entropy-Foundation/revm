f1d28462e369163199149e8197510b17ef367d6c
Aregnaz Harutyunyan <>
2025-12-29 16:04:22 +0400

    Disabled main logic of supra-extensions bindings generation
    - If needed it should be enabled and generated manually

--- crates/supra-extension/build.rs
+++ crates/supra-extension/build.rs
@@ -1,8 +1,6 @@
 use std::env;
 use std::path::PathBuf;
 
-use clap::Parser;
-use forge::cmd::bind::BindArgs;
 
 fn main() {
     // 1. Tell Cargo to rerun the script if the contracts directory changes
@@ -14,27 +12,37 @@ fn main() {
         cargo_dir.display()
     );
 
-    // Determine the output directory for the generated bindings
+    // smr-moonshot referencing this version of the REVM has dependency conflicts caused by syn library used by forge.
+    // So unless the issue is fixed, rust bindings on updates of the SupraContractsBindings.sol will be regenerated manually
+    // and committed. This code should remain commented out otherwise
+    // To do it:
+    // - uncomment below code
+    // - uncomment build dependencies in Cargo.toml file of this project
+    // - build the project
 
-    let contracts_relative_path = PathBuf::from("../../solidity/supra_contracts");
-    let contracts_build_config =
-        cargo_dir.join(contracts_relative_path.join(PathBuf::from("foundry.toml")));
-    let contract_names = "SupraContracts";
 
-    let bindings_path = cargo_dir
-        .join(PathBuf::from("src"))
-        .join(PathBuf::from("supra_contract_bindings"));
+    //// Determine the output directory for the generated bindings
+    //use clap::Parser;
+    //use forge::cmd::bind::BindArgs;
+    //let contracts_relative_path = PathBuf::from("../../solidity/supra_contracts");
+    //let contracts_build_config =
+    //    cargo_dir.join(contracts_relative_path.join(PathBuf::from("foundry.toml")));
+    //let contract_names = "SupraContracts";
 
-    // Ensure the output directory exists
-    std::fs::create_dir_all(bindings_path.as_path()).expect("Failed to create bindings directory");
-    let command_inputs = format!(
-        "bind --bindings-path {} --overwrite --module --select {} --alloy --config-path {}",
-        bindings_path.display(),
-        contract_names,
-        contracts_build_config.display()
-    );
-    let parsed_inputs = shlex::split(&command_inputs).expect("Failed to parse command string");
-    let bind_cmd: BindArgs =
-        BindArgs::try_parse_from(parsed_inputs).expect("Failed to parse command arguments");
-    bind_cmd.run().expect("Failed to execute bind command");
+    //let bindings_path = cargo_dir
+    //    .join(PathBuf::from("src"))
+    //    .join(PathBuf::from("supra_contract_bindings"));
+
+    //// Ensure the output directory exists
+    //std::fs::create_dir_all(bindings_path.as_path()).expect("Failed to create bindings directory");
+    //let command_inputs = format!(
+    //    "bind --bindings-path {} --overwrite --module --select {} --alloy --config-path {}",
+    //    bindings_path.display(),
+    //    contract_names,
+    //    contracts_build_config.display()
+    //);
+    //let parsed_inputs = shlex::split(&command_inputs).expect("Failed to parse command string");
+    //let bind_cmd: BindArgs =
+    //    BindArgs::try_parse_from(parsed_inputs).expect("Failed to parse command arguments");
+    //bind_cmd.run().expect("Failed to execute bind command");
 }
