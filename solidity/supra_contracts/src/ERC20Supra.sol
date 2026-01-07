40c2b562d00de21eaa1520955ae6bbbb404f2666
Udit Yadav <u.yadav@supraoracles.com>
2026-01-07 18:17:17 +0530

    Applies the changes from feature/erc20 supra (#12)
    * added smart contracts, scripts and tests for multisig
    
    * -fixed deployContract to allow deployment using multisig as msg.sender
    -added test cases for deployContract
    
    * added test cases for receive
    
    * -resolved PR comments
    
    * moved Counter to tests
    
    * Renamed solidity/automation_registry -> solidity/supra_contracts
    
    * Added missing files for build and tests
    
    * added erc20Supra smart contract and test cases
    
    * moved SC and tests to supra_contracts
    
    * updated .gitignore and import statment in test file
    
    * updated .gitignore and added deployment script
    
    * renamed test file
    
    * renamed events and functions
    
    * updated Counter and tests associated with it
    
    * updated comments
    
    * added transaction deletion
    
    * updated to remove expired txs
    
    * renamed test cases
    
    ---------
    
    Co-authored-by: Aregnaz Harutyunyan <>

---
+++ solidity/supra_contracts/src/ERC20Supra.sol
@@ -0,0 +1,70 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.27;
+
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {ERC20Burnable} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import {ERC20Permit} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
+import "@openzeppelin/contracts/access/Ownable2Step.sol";
+
+contract ERC20Supra is ERC20, ERC20Burnable, Ownable2Step, ERC20Permit {
+    
+    /// @notice Error thrown if user has insufficient balance.
+    error InsufficientBalance();
+    /// @notice Error thrown if 0 is passed as amount.
+    error InvalidAmount();
+    /// @notice Error thrown if tokens are sent to the token contract itself.
+    error InvalidTransfer();
+    /// @notice Error thrown if low level call fails.
+    error TransferFailed();
+
+    /// @notice Emitted when native tokens are deposited to mint and receive ERC20Supra tokens.
+    /// @param account Address of the depositer.
+    /// @param amount Amount deposited.
+    event NativeToERC20Supra(address indexed account, uint256 indexed amount);
+    
+    /// @notice Emitted when native tokens are withdrawn by burning ERC20Supra tokens. 
+    /// @param account Address withdrawing.
+    /// @param amount Amount withdrawn.
+    event ERC20SupraToNative(address indexed account, uint256 indexed amount);
+
+    constructor(address _initialOwner)
+        ERC20("ERC20Supra", "SUPRA")
+        Ownable(_initialOwner)
+        ERC20Permit("ERC20Supra")
+    {}
+
+    /// @notice Deposit native token → Mint ERC20Supra 1:1
+    function nativeToErc20Supra() external payable {
+        if (msg.value == 0) revert InvalidAmount();
+        _mint(msg.sender, msg.value);
+
+        emit NativeToERC20Supra(msg.sender, msg.value);
+    }
+
+    /// @notice Withdraw native token → Burn ERC20Supra 1:1
+    /// @param _amount Amount of native tokens to withdraw.
+    function erc20SupraToNative(uint256 _amount) external {
+        if (_amount == 0) revert InvalidAmount();
+        if (balanceOf(msg.sender) < _amount) revert InsufficientBalance();
+        
+        _burn(msg.sender, _amount);
+        emit ERC20SupraToNative(msg.sender, _amount);
+
+        (bool sent, ) = payable(msg.sender).call{value: _amount}("");
+        if (!sent) revert TransferFailed();
+    }    
+
+    /// @notice Allows a user to send native tokens directly and get ERC20Supra.
+    receive() external payable {
+        if (msg.value == 0) revert InvalidAmount();
+        
+        _mint(msg.sender, msg.value);
+        emit NativeToERC20Supra(msg.sender, msg.value);
+    }
+
+    /// @notice Disallows sending tokens to the token contract itself. This prevents accidental locking of tokens.
+    function _update(address _from, address _to, uint256 _value) internal override {
+        if (_to == address(this)) revert InvalidTransfer();
+        super._update(_from, _to, _value);
+    }
+}
