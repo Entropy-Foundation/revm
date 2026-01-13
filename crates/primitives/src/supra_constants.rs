f83e999c6151cc771b529baa85b2a0e5efc373a0
Aregnaz Harutyunyan <>
2026-01-13 12:07:25 +0400

    Addressed review comments

--- crates/primitives/src/supra_constants.rs
+++ crates/primitives/src/supra_constants.rs
@@ -1,22 +1,6 @@
 //! Constants defined by SUPRA to facilitate execution flow extensions.
 use alloy_primitives::Address;
 
-/// Defines a constant of Address type for the input value with the specified name
-#[macro_export]
-macro_rules! define_reserved_addresses {
-    (
-        $(
-            $doc:expr,
-            $name:ident = $value:expr
-        ),+ $(,)?
-    ) => {
-        $(
-            #[doc = $doc]
-            pub const $name: Address = $value;
-        )+
-    };
-}
-
 /// Converts [`u64`] to [`Address`] type.
 pub const fn u64_to_address(x: u64) -> Address {
     let x = x.to_be_bytes();
@@ -25,27 +9,19 @@ pub const fn u64_to_address(x: u64) -> Address {
     ])
 }
 
-/// Generates continues range of address.
-///  - start is the start address
-///  - N generic parameter specifying the length of the range from the specified start.
-pub const fn generate_address_range<const N: usize>(start: u64) -> [Address; N] {
-    let start_address = u64_to_address(start);
-    let mut reserved_addresses: [Address; N] =  [start_address; N];
-    let mut i = 1;
-    while i < N {
-        let address = u64_to_address(start + i as u64);
-        reserved_addresses[i]=  address;
-        i += 1;
-    }
-    reserved_addresses
-}
+/// Supra Reserved address for VM SIGNER,
+pub const VM_SIGNER: Address = u64_to_address(0x5355_5000);
+
+/// Supra Reserved address Precompile address to retrieve transaction hash
+pub const TX_HASH_ADDRESS: Address = u64_to_address(0x5355_5001);
 
 /// [0x5355_5000, 0x53555_50FF] addresses are reserved as SUPRA special addresses.
-pub const SUPRA_RESERVED_ADDRESSES: [Address; 0xFF] = generate_address_range::<0xff>(0x5355_5000);
+const SUPRA_RESERVED_ADDRESSES_PREFIX_UPPER_BOUND: usize = 19;
 
-/// Checks whether specified input address is one of the SUPRA
+/// Checks whether specified input address is one of the SUPRA reserved ones.
 pub fn is_supra_reserved(address: &Address) -> bool {
-    SUPRA_RESERVED_ADDRESSES.contains(address)
+    VM_SIGNER[..SUPRA_RESERVED_ADDRESSES_PREFIX_UPPER_BOUND]
+        .eq(&address[..SUPRA_RESERVED_ADDRESSES_PREFIX_UPPER_BOUND])
 }
 
 /// Checks whether specified input address is SUPRA reserved VM_SIGNER
@@ -53,10 +29,22 @@ pub fn is_vm_signer(address: &Address) -> bool {
     VM_SIGNER.eq(address)
 }
 
-define_reserved_addresses!(
-    "Supra Reserved address for VM SIGNER",
-    VM_SIGNER = SUPRA_RESERVED_ADDRESSES[0],
-    "Supra Reserved address Precompile address to retrieve transaction hash",
-    TX_HASH_ADDRESS = SUPRA_RESERVED_ADDRESSES[1],
-);
+#[cfg(test)]
+mod tests {
+    use crate::supra_constants::{is_supra_reserved, TX_HASH_ADDRESS, VM_SIGNER};
 
+    #[test]
+    fn check_reserved_addresses() {
+        let addr5 = super::u64_to_address(0x5355_5005);
+        let last_reserved = super::u64_to_address(0x5355_50ff);
+        let any_low_address = super::u64_to_address(0x5355_4fff);
+        let any_up_address = super::u64_to_address(0x5355_5100);
+        let any_address = super::u64_to_address(0x1_5355_5000);
+        assert!(is_supra_reserved(&addr5));
+        assert!(is_supra_reserved(&VM_SIGNER));
+        assert!(is_supra_reserved(&TX_HASH_ADDRESS));
+        assert!(!is_supra_reserved(&any_address));
+        assert!(!is_supra_reserved(&any_low_address));
+        assert!(!is_supra_reserved(&any_up_address));
+    }
+}
